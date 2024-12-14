cd349cdfd5c409c2196394b53a5ba2a6c598ac4e
Soumay-sanpui <somusanpui9@gmail.com>
2024-12-14 22:19:16 +0530

    add user registration & login

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import { connectDB } from "./utilities/db.util.js";
+import authRoutes from "./routes/auth.routes.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
@@ -10,7 +11,13 @@ app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
-connectDB().then(() => {
+app.get("/", (req, res) => {
+  res.send("Hello World, from NLRS Backend.");
+});
+
+app.use("/api/v1/auth", authRoutes);
+
+await connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
